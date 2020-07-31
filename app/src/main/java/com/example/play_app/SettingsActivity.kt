a5f6e2bb8ad95e7bf14d8e8608ada5c1b88feaa2
YE-EUN <musiclove529@naver.com>
2020-08-01 06:35:10 +0900

    뒤로가기 버튼 수정

--- app/src/main/java/com/example/play_app/SettingsActivity.kt
+++ app/src/main/java/com/example/play_app/SettingsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -11,26 +12,18 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backButton = findViewById<ImageButton>(R.id.settings_back_button)
-        val appInfoButton = findViewById<ImageButton>(R.id.app_info_button)
-        val playListButton = findViewById<ImageButton>(R.id.play_list_button)
-
-        backButton.setOnClickListener {
-            val intent = Intent(applicationContext, HomeActivity::class.java)
-            startActivity(intent)
+        settings_back_button.setOnClickListener {
             finish()
         }
 
-        appInfoButton.setOnClickListener {
-            val intent = Intent(applicationContext, AppInfoActivity::class.java)
+        app_info_button.setOnClickListener {
+            val intent = Intent(this, AppInfoActivity::class.java)
             startActivity(intent)
-            finish()
         }
 
-        playListButton.setOnClickListener {
-            val intent = Intent(applicationContext, PlayListActivity::class.java)
+        play_list_button.setOnClickListener {
+            val intent = Intent(this, PlayListActivity::class.java)
             startActivity(intent)
-            finish()
         }
     }
 }
