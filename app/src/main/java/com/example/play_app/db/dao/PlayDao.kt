45ec2ed0a49b5bfc5b6ac5be0058f1a5cd276413
YE-EUN <musiclove529@naver.com>
2020-08-14 23:29:55 +0900

    주석제거

--- app/src/main/java/com/example/play_app/db/dao/PlayDao.kt
+++ app/src/main/java/com/example/play_app/db/dao/PlayDao.kt
@@ -17,30 +17,6 @@ interface PlayDao {
             "LIMIT 1")
     fun getResult(indoor:String?,outdoor:String?,free:String?,pay:String?,alone:String?,friend:String?,active:String?,inactive:String?):Play
 
-//    @Query("SELECT * FROM play WHERE place = '실내'")
-//    fun getIndoor():List<Play>
-//
-//    @Query("SELECT * FROM play WHERE place = '실외'")
-//    fun getOutdoor():List<Play>
-//
-//    @Query("SELECT * FROM play WHERE cost = '무료'")
-//    fun getFree():List<Play>
-//
-//    @Query("SELECT * FROM play WHERE cost = '유료'")
-//    fun getPay():List<Play>
-//
-//    @Query("SELECT * FROM play WHERE num = '혼자서'")
-//    fun getAlone():List<Play>
-//
-//    @Query("SELECT * FROM play WHERE num = '여럿이서'")
-//    fun getFriend():List<Play>
-//
-//    @Query("SELECT * FROM play WHERE act = '활동적'")
-//    fun getActive():List<Play>
-//
-//    @Query("SELECT * FROM play WHERE act = '비활동적'")
-//    fun getInActive():List<Play>
-
     @Insert
     fun insert(play: Play)
 
